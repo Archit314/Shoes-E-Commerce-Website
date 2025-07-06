bfbe97750c8ca9af791e5cd55f17747f8557fff9
Archit Kumar <architkumar314@gmail.com>
2025-07-06 22:37:59 +0530

    Backend - Added Sequelize setup with packages: pg-hstore, sequelize & sequelize-cli

---
+++ app_backend/src/config/sequelize.config.js
@@ -0,0 +1,23 @@
+// src/app/config/sequelize.config.js
+
+import dotenv from 'dotenv';
+dotenv.config();
+import fs from 'fs';
+
+export default {
+  development: {
+    username: process.env.DB_USER,       // should be 'username', not 'user'
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+        ca: fs.readFileSync('./ca.pem').toString()
+      }
+    }
+  }
+};
