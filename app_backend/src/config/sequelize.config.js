372078d30a6e81184976b2db2ba94f61e14de7e3
Archit Kumar <architkumar314@gmail.com>
2025-07-07 16:09:31 +0530

    Backend Fixes - Added fixes for production sequelize setting

--- app_backend/src/config/sequelize.config.js
+++ app_backend/src/config/sequelize.config.js
@@ -22,12 +22,17 @@ export default {
   },
 
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    username: process.env.DB_USER,       // should be 'username', not 'user'
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false // Render requires this to be false
+        rejectUnauthorized: true,
+        ca: fs.readFileSync('./ca.pem').toString()
       }
     }
   }
