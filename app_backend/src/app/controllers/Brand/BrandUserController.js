789a664f9a65e38eb42f7cab96bfda6a84692a62
Archit Kumar <architkumar314@gmail.com>
2025-07-12 21:19:23 +0530

    Backend - Added getBrands API for User.

---
+++ app_backend/src/app/controllers/Brand/BrandUserController.js
@@ -0,0 +1,18 @@
+import BrandUserService from "../../services/Brand/BrandUserService.js";
+
+export const getBrands = async (req, res) => {
+
+    try {
+        
+        const brandUserService = new BrandUserService()
+        const getBrands = await brandUserService.getBrands()
+        if(getBrands.status != 200){
+            return res.status(422).json({ status: getBrands.status, message: getBrands.message });
+        }
+
+        return res.status(200).json({ status: getBrands.status, message: getBrands.message, data: getBrands.data });
+    } catch (error) {
+        console.error('[BrandUserController]: Error in getBrands:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
---
+++ app_backend/src/app/services/Brand/BrandUserService.js
@@ -0,0 +1,20 @@
+const db = await import('../../../models/index.js')
+const {Brand} = db.default
+
+export default class BrandUserService {
+
+    async getBrands() {
+
+        try {
+            const brands = await Brand.findAll();
+            if(brands.length === 0){
+                return {status: 422, message: 'No brands found'}
+            }
+    
+            return {status: 200, message: 'brands fetched successfully', data: brands}
+        } catch (error) {
+            console.error('[BrandUserService]: Error in getBrands:', error);
+            return {status: 500, message: 'Internal Server Error'};
+        }
+    }
+}
