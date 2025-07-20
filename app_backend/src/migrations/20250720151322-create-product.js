918a0d113940fd06796782b0a497086937532fdc
Archit Kumar <architkumar314@gmail.com>
2025-07-20 22:20:50 +0530

    Backend - Added Products table.

---
+++ app_backend/src/migrations/20250720151322-create-product.js
@@ -0,0 +1,53 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      brand_id: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: 'Brands',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      category_id: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: 'Categories',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ app_backend/src/models/product.js
@@ -0,0 +1,66 @@
+'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+import { DataTypes, Model } from 'sequelize';
+
+export default (sequelize) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+
+      // Define associations with category:
+      Product.belongsTo(models.Category, {
+        foreignKey: 'category_id',
+        as: 'category'
+      });
+
+      // Define associations with product brand:
+      Product.belongsTo(models.Brand, {
+        foreignKey: 'brand_id',
+        as: 'brand'
+      });
+
+      // Define association with product variants:
+      // Product.hasMany(models.ProductVariant, {
+      //   foreignKey: 'product_id',
+      //   as: 'variants'
+      // });
+    }
+  }
+  Product.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    brand_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    is_active: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Product',
+    tableName: 'products',
+    timestamps: true
+  });
+  return Product;
+};
