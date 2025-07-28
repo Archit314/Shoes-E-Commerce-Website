edad8963ad24633feae8a01d9a4076c44cb3059f
Archit Kumar <architkumar314@gmail.com>
2025-07-28 18:33:36 +0530

    Backend - Update cashfree config

--- app_backend/src/app/config/cashfree/cashfree.js
+++ app_backend/src/app/config/cashfree/cashfree.js
@@ -1,5 +1,6 @@
 export const cashfreeConfig = {
     cf_client_id: process.env.CF_X_CLIENT_ID, 
     cf_secret_key: process.env.CF_X_SECRET_KEY,
-    cf_api_version: process.env.CF_API_VERSION
+    cf_api_version: process.env.CF_API_VERSION,
+    cf_create_order_url: process.env.CF_CREATE_ORDER
 }
