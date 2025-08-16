3063440594fdadb9af0ac7fa28e1c6c815db5576
Archit Kumar <architkumar314@gmail.com>
2025-08-16 15:55:03 +0530

    Backend - Added CORS.

--- app_backend/src/server.js
+++ app_backend/src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import express from 'express';
+import cors from "cors"
 const app = express();
 
 import { connectDb } from './app/provider/aiven database/databaseConnection.js';
@@ -20,6 +21,10 @@ import orderUserRoutes from './start/routes/Order/orderUserRoutes.js';
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+  origin: process.env.FRONTEND_ORIGIN,
+  credentials: true
+}))
 
 app.get('/ping', (req, res) => {
   console.log('[Server]: Ping route hit');
