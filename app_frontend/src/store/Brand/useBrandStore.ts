2242387142e070c1803e4bd145c9ec0cbacb3783
Archit Kumar <architkumar314@gmail.com>
2025-08-23 15:24:45 +0530

    Frontend - Added useBrandStore

--- app_frontend/src/store/Brand/useBrandStore.ts
+++ app_frontend/src/store/Brand/useBrandStore.ts
@@ -1,11 +1,55 @@
 import {create} from "zustand"
+import { axiosInstance } from "../../lib/axios"
 
-export const useBrandStore = create((set) => ({
+
+type BrandStoreResponse = {
+    status: number,
+    message: string,
+    data: []
+}
+interface BrandStore {
+    isGettingBrands: boolean,
+    brands: any[],
+    getBrands: () => Promise<BrandStoreResponse>
+}
+
+export const useBrandStore = create<BrandStore>((set) => ({
 
     isGettingBrands: false,
     brands: [],
 
     getBrands: async () => {
         set({isGettingBrands: true})
+
+        try {
+
+            const gotBrands = await axiosInstance.get('/user/brand')
+
+            set({isGettingBrands: false, brands: gotBrands.data.data})
+
+            return {
+                status: gotBrands.data.status,
+                message: gotBrands.data.message,
+                data: gotBrands.data.data
+            }
+            
+        } catch (error: any) {
+            console.log(error);
+            set({isGettingBrands: false, brands: []})
+
+            if(error.status == 422){
+                return {
+                    status: error.response.data.status,
+                    message: error.response.data.message,
+                    data: error.response.data.data
+                }
+            }
+
+            return{
+                status: 500,
+                message: error.message,
+                data: []
+            }
+        }
     }
 }))
