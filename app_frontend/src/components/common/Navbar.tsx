ef23e19e46d949458636c11ec04a59680c16e236
Archit Kumar <architkumar314@gmail.com>
2025-10-05 14:16:30 +0530

    Frontend - Added Navbar UI changes & navigation.

--- app_frontend/src/components/common/Navbar.tsx
+++ app_frontend/src/components/common/Navbar.tsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 import { ShoppingBag, Menu, X, LogInIcon } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthStore } from "../../store/User/useAuthStore";
 
 function Navbar() {
+  const navigate = useNavigate()
+  const {authUser} = useAuthStore()
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleLoginClick = () => {
+    navigate('/sign-up')
+  }
 
   return (
     <nav className="sticky top-0 z-50 bg-black/50 backdrop-blur-md shadow-lg px-6 py-4 flex justify-between items-center rounded-b-2xl border-b border-white/20">
@@ -22,17 +29,27 @@ function Navbar() {
       </ul>
 
       {/* Auth Button - Desktop */}
-      <div className="hidden md:block">
-        <button className="flex items-center gap-2 bg-white text-pink-600 hover:bg-pink-600 hover:text-white px-6 py-2 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105 font-semibold">
+      {!authUser? <div className="hidden md:block">
+        <button
+          onClick={handleLoginClick}
+          className="flex items-center gap-2 bg-white text-pink-600 hover:bg-pink-600 hover:text-white px-6 py-2 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105 font-semibold"
+        >
           <LogInIcon />
         </button>
-      </div>
+      </div>: <div className="hidden md:block">
+        <button className="flex items-center gap-2 bg-white text-pink-600 hover:bg-pink-600 hover:text-white px-6 py-2 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105 font-semibold">
+          "asdfsdf"
+        </button>
+      </div>}
 
       {/* Mobile Menu Button */}
       <div className="md:hidden flex items-center gap-4">
-        <button className="bg-white text-pink-600 hover:bg-pink-600 hover:text-white p-2 rounded-full shadow-lg transition-all duration-300">
+        {!authUser? <button className="bg-white text-pink-600 hover:bg-pink-600 hover:text-white p-2 rounded-full shadow-lg transition-all duration-300">
           <LogInIcon size={20} />
-        </button>
+        </button>:
+        <button className="bg-white text-pink-600 hover:bg-pink-600 hover:text-white p-2 rounded-full shadow-lg transition-all duration-300">
+          "afsadfsdf"
+        </button>}
         <button
           className="text-white hover:text-pink-300 transition-colors"
           onClick={() => setIsOpen(!isOpen)}
@@ -45,10 +62,10 @@ function Navbar() {
       {isOpen && (
         <div className="absolute top-full left-0 w-full bg-black/80 backdrop-blur-lg md:hidden flex flex-col items-center space-y-6 py-6 text-white font-medium animate-fadeIn">
           <Link className="list-none hover:text-pink-300 cursor-pointer" to={"/"}>Home</Link>
-          <Link className="list-none hover:text-pink-300 cursor-pointer" to={"/"}>Shop</Link>
+          <Link className="list-none hover:text-pink-300 cursor-pointer" to={"/shop"}>Shop</Link>
           <Link className="list-none hover:text-pink-300 cursor-pointer" to={"/about"}>About</Link>
           <Link className="list-none hover:text-pink-300 cursor-pointer" to={"/"}>Contact</Link>
-          <Link className="list-none hover:text-pink-300 cursor-pointer" to={"/"}><ShoppingBag/></Link>
+          <Link className="list-none hover:text-pink-300 cursor-pointer" to={"/cart"}><ShoppingBag/></Link>
         </div>
       )}
     </nav>
